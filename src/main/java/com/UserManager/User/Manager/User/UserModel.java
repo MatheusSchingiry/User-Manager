76dde83a8cdadf94f6cfcd18fb3849c84db4e2d0
Matheus_PC <matheussschingiry@gmail.com>
2025-04-29 19:19:51 -0300

    fix: User Model unique email

--- src/main/java/com/UserManager/User/Manager/User/UserModel.java
+++ src/main/java/com/UserManager/User/Manager/User/UserModel.java
@@ -16,6 +16,8 @@ public class UserModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
 }
