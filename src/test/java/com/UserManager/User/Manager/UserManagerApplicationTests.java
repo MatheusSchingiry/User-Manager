88be09b7dd5eec6d0a4ba5ed03e211093973545f
Matheus_PC <matheussschingiry@gmail.com>
2025-04-29 18:44:00 -0300

    first commit

---
+++ src/test/java/com/UserManager/User/Manager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserManager.User.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
