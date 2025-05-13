4a6f87c9abba83dc9cd5352a92a1d0307c6068c7
Matheus <matheussschingiry@gmail.com>
2025-05-13 20:51:12 -0300

    refactor: User repository name variable with lowercase

--- src/main/java/com/UserManager/User/Manager/User/UserRepository.java
+++ src/main/java/com/UserManager/User/Manager/User/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, String> {
-    Optional<UserModel> findByEmail(String Email);
+    Optional<UserModel> findByEmail(String email);
 }
