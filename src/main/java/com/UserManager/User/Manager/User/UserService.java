0f6e72ee6e215039b58d724ebff7f383ecf38b62
Matheus <matheussschingiry@gmail.com>
2025-08-14 22:10:30 -0300

    feat: list all user by ID

--- src/main/java/com/UserManager/User/Manager/User/UserService.java
+++ src/main/java/com/UserManager/User/Manager/User/UserService.java
@@ -1,53 +1,57 @@
-package com.UserManager.User.Manager.User;
+    package com.UserManager.User.Manager.User;
 
 
-import com.UserManager.User.Manager.Exception.UserNotFoundException;
-import org.springframework.stereotype.Service;
+    import com.UserManager.User.Manager.Exception.UserNotFoundException;
+    import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+    import java.util.List;
+    import java.util.stream.Collectors;
 
-@Service
-public class UserService {
+    @Service
+    public class UserService {
 
-    private final UserRepository repository;
-    public final UserMapper mapping;
+        private final UserRepository repository;
+        public final UserMapper mapping;
 
-    public UserService(UserRepository repository, UserMapper mapping) {
-        this.repository = repository;
-        this.mapping = mapping;
+        public UserService(UserRepository repository, UserMapper mapping) {
+            this.repository = repository;
+            this.mapping = mapping;
+        }
+
+        public List<UserDTO> listUser(){
+            return repository.findAll().stream()
+                    .map(mapping::toDTO)
+                    .collect(Collectors.toList());
+        }
+
+        public UserDTO listUserById(String id){
+            return repository.findById(id).map(mapping::toDTO).orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
+        }
+
+        public UserDTO listUserByEmail(String email){
+            return repository.findByEmail(email).map(mapping::toDTO).orElseThrow(() -> new UserNotFoundException("User not found with email: " + email));
+        }
+
+        public UserDTO addUser(UserDTO dto){
+            UserModel model = mapping.toModel(dto);
+            model = repository.save(model);
+            return mapping.toDTO(model);
+        }
+
+        public void deleteUser(String id){
+            repository.deleteById(id);
+        }
+
+        public UserDTO editUser(String id, UserDTO dto){
+            UserModel existingUser = repository.findById(id)
+                    .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
+
+            if (dto.getName() == null) dto.setName(existingUser.getName());
+            if (dto.getEmail() == null) dto.setEmail(existingUser.getEmail());
+            if (dto.getPassword() == null) dto.setPassword(existingUser.getPassword());
+
+            dto.setId(id);
+            UserModel updatedUser = repository.save(mapping.toModel(dto));
+            return mapping.toDTO(updatedUser);
+        }
     }
-
-    public List<UserDTO> listUser(){
-        return repository.findAll().stream()
-                .map(mapping::toDTO)
-                .collect(Collectors.toList());
-    }
-
-    public UserDTO listUserByEmail(String email){
-        return repository.findByEmail(email).map(mapping::toDTO).orElseThrow(() -> new UserNotFoundException("User not found with ID: " + email));
-    }
-
-    public UserDTO addUser(UserDTO dto){
-        UserModel model = mapping.toModel(dto);
-        model = repository.save(model);
-        return mapping.toDTO(model);
-    }
-
-    public void deleteUser(String id){
-        repository.deleteById(id);
-    }
-
-    public UserDTO editUser(String id, UserDTO dto){
-        UserModel existingUser = repository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
-
-        if (dto.getName() == null) dto.setName(existingUser.getName());
-        if (dto.getEmail() == null) dto.setEmail(existingUser.getEmail());
-        if (dto.getPassword() == null) dto.setPassword(existingUser.getPassword());
-
-        dto.setId(id);
-        UserModel updatedUser = repository.save(mapping.toModel(dto));
-        return mapping.toDTO(updatedUser);
-    }
-}
